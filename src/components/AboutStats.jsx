2883d89083d400ec3c573754dfb4960274ec9c7c
edlorenz2018 <edlorenzvil2018@gmail.com>
2026-03-12 11:07:08 +0800

    updated team members

--- src/components/AboutStats.jsx
+++ src/components/AboutStats.jsx
@@ -5,6 +5,16 @@ import CountUp from "react-countup";
 import CEO from "../assets/kemariceo.png"
 import FINANCE from "../assets/finance.png"
 import HR from "../assets/hrofficer.png"
+import ACC from "../assets/accountingofficer.png"
+import ADMIN from "../assets/Admin.png"
+import CAD from "../assets/cad.png"
+import DRIVER from "../assets/driver.png"
+import HEAD from "../assets/head.png"
+import PERSONNEL from "../assets/personnel.png"
+import SURVEYOR from "../assets/quantitysurveyor.png"
+import SITE1 from "../assets/site1.png"
+import SITE2 from "../assets/site2.png"
+import SUPERVISOR from "../assets/superviser.png"
 function AboutStats() {
   return (
     <section className="about-stats">
@@ -117,80 +127,109 @@ function AboutStats() {
       </p>
     </div>
   </div>
-
 </div>
-    {/* TEAM SECTION */}
-<div className="team2-section">
+      {/* OUR TEAM SECTION */}
+<div className="team-section">
 
-  <h2 className="team2-phrase">
-    Our strength lies in the people behind the systems a team driven by
-    innovation, collaboration, and technical excellence.
-  </h2>
+  <div className="team-header">
+    <span className="team-small">Our Team</span>
+    <h2>Meet our <span>visionary team</span></h2>
 
-  <div className="team2-grid">
-
-    {/* MEMBER */}
-    <div className="team2-card">
-
-      <div className="team2-avatar">
-        <img src={CEO} alt="member"/>
-      </div>
-      <h3>Loreke V. Burbos</h3>
-      <p className="team2-role">
-        CEO/PRESIDENT
-      </p>
-
-      <p className="team2-desc">
-        The CEO of Kemari guides the company in providing reliable 
-        and well-structured technology systems for businesses
-         and institutions.
-      </p>
-
-      
-    </div>
-
-
-    <div className="team2-card">
-  
-      <div className="team2-avatar">
-        <img src={FINANCE} alt="member"/>
-      </div>
-
-      <h3>Olivia Bennett</h3>
-      <p className="team2-role">
-        Finance Manager.
-      </p>
-
-      <p className="team2-desc">
-       The Finance Manager at Kemari oversees financial planning
-        and management, ensuring the company’s resources are used 
-        efficiently to support growth and stable operations.
-      </p>
-
-      
-    </div>
-
-
-    <div className="team2-card">
-    
-      <div className="team2-avatar">
-        <img src={HR} alt="member"/>
-      </div>
-
-      <h3>Ferdilisa V. Sulat</h3>
-      <p className="team2-role">
-        HR OFFICER
-      </p>
-
-      <p className="team2-desc">
-        The HR Officer at Kemari manages human resources,
-         fostering a supportive work environment and ensuring
-          effective recruitment, development, and employee well-being.
-      </p>
-
-    </div>
+    <p>
+      Our team is our greatest asset. We bring together a diverse group of 
+      talented professionals with a shared passion for technology and innovation.
+    </p>
   </div>
+
+  <div className="team-grid">
+
+    <div className="team-card">
+      <img src={CEO} alt="CEO" />
+      <h4>Loreke V. Burbos</h4>
+      <p>CEO/PRESIDENT</p>
+    </div>
+
+    <div className="team-card">
+      <img src={FINANCE} alt="CFO" />
+      <h4>Maricel S. Burbos</h4>
+      <p>FINANCE MANAGER</p>
+    </div>
+
+    <div className="team-card">
+      <img src={HR} alt="CTO" />
+      <h4>Ferdilisa V. Sulat</h4>
+      <p>HR OFFICER</p>
+    </div>
+
+    <div className="team-card">
+      <img src={ACC} alt="CMO" />
+      <h4>Donabel V. Tapdasan</h4>
+      <p>ACCOUNTING OFFICER</p>
+    </div>
+
+    <div className="team-card">
+      <img src={ADMIN} alt="Advisor" />
+      <h4>Maria Fatima B. Tapic</h4>
+      <p>ADMIN/PURCHASING</p>
+    </div>
+
+    <div className="team-card">
+      <img src={DRIVER} alt="Operations" />
+      <h4>Rosauro T. Sabellon</h4>
+      <p>PURCHASING DRIVER</p>
+    </div>
+
+    <div className="team-card">
+      <img src={SURVEYOR} alt="Marketing" />
+      <h4>Rachel Marie L. Miranda</h4>
+      <p>QUANTITY SURVEYOR</p>
+    </div>
+
+    <div className="team-card">
+      <img src={SUPERVISOR} alt="Engineer" />
+      <h4>Ramil Handumon</h4>
+      <p>PROJECT SUPERVISOR</p>
+    </div>
+    <div className="team-card">
+      <img src={CAD} alt="Engineer" />
+      <h4>Jogie Anne T. Candol</h4>
+      <p>CAD OPERATOR</p>
+    </div>
+    <div className="team-card">
+      <img src={SITE2} alt="Engineer" />
+      <h4>Princess Lea Canlas</h4>
+      <p>SITE ENGINEER/PASEO</p>
+    </div>
+    <div className="team-card">
+      <img src={SITE1} alt="Engineer" />
+      <h4>Reynzo N. Olila</h4>
+      <p>SITE ENGINEER/VERTEX</p>
+    </div>
+    <div className="team-card">
+      <img src={HEAD} alt="Engineer" />
+      <h4>Mario L. Aparis</h4>
+      <p>TECHNICAL HEAD</p>
+    </div>
+    <div className="team-card">
+      <img src={PERSONNEL} alt="Engineer" />
+      <h4>Angelito N. Abalo</h4>
+      <p>TECHNICAL PERSONNEL/OUTSIDE CEBU</p>
+    </div>
+    <div className="team-card">
+      <img src={HEAD} alt="Engineer" />
+      <h4>Mario L. Aparis</h4>
+      <p>TECHNICAL HEAD</p>
+    </div>
+    <div className="team-card">
+      <img src={HEAD} alt="Engineer" />
+      <h4>Mario L. Aparis</h4>
+      <p>TECHNICAL HEAD</p>
+    </div>
+
+  </div>
+
 </div>
+
     </section>
   );
 }
